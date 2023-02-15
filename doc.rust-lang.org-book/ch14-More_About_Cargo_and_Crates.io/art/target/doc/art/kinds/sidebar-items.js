fc509d7441acb67d568b2e670b4e14a97061b68c
Deni Andrian Prayoga <deniandriancode@gmail.com>
2023-02-15 20:53:43 +0700

    init

---
+++ doc.rust-lang.org-book/ch14-More_About_Cargo_and_Crates.io/art/target/doc/art/kinds/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["PrimaryColor","The primary colors according to RYB color model"],["SecondaryColor","The secondary colors according to RYB color model"]]};
---
+++ doc.rust-lang.org-book/ch14-More_About_Cargo_and_Crates.io/art/target/doc/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"art":["",[],["lib.rs"]]\
+}');
+createSourceSidebar();
